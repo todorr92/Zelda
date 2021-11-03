360fb9b621ce35570521bd98d6c86ecd574b78b1
unknown <todorovicslobodan10@gmail.com>
2021-11-03 08:08:20 +0000

    reformatted skeletor and slicer colliding with wall

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 kaboom({
   global: true,
   fullscreen: true,
-  scale: 2,
+  scale: 1,
   debug: true,
   clearColor: [0, 0, 1, 1],
 })
@@ -171,10 +171,6 @@ scene('game', ({ level, score }) => {
     s.move(s.dir * SLICER_SPEED, 0)
   })
 
-  collides('slicer', 'wall', (s) => {
-    s.dir = -s.dir
-  })
-
   action('skeletor', (s) => {
     s.move(0, s.dir * SKELETOR_SPEED)
     s.timer -= dt()
@@ -184,7 +180,7 @@ scene('game', ({ level, score }) => {
     }
   })
 
-  collides('skeletor', 'wall', (s) => {
+  collides('dangerous', 'wall', (s) => {
     s.dir = -s.dir
   })
 
